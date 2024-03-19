69f183fd60498618ddf388284fb474859fbee083
Nishika2307 <mouriyanishika23@gmail.com>
2024-03-20 00:54:54 +0530

    Posting question 6

---
+++ PART 2/q6.java
@@ -0,0 +1,23 @@
+import java.util.*; //TreeSet
+public class q6 
+{
+  public static void main(String[] args) 
+   {
+	  Scanner sc=new Scanner(System.in);
+	  TreeSet<Integer> ts=new TreeSet<Integer>();//can store only one datatype
+	    ts.add(10);
+		ts.add(10);
+		ts.add(5);
+		ts.add(7);
+		ts.add(20);
+		System.out.println(ts);
+		int n;
+		System.out.print("Enter the number to be searched : ");
+		n=sc.nextInt();
+		System.out.println(ts.contains(n));
+		ts.remove(7);
+		System.out.println("After Removal 7 "+ts);
+		
+}
+
+}
